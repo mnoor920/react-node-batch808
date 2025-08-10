8c7f474ff74877dc80863200f93e3a6c5ed4705d
mnoor920 <mnoor92081@hotmail.com>
2025-08-10 20:08:44 +0500

    style setup

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -3,20 +3,47 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <footer className="App-footer">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <footer>
+        <div className="footer-area">
+          <div className="container">
+            <div className="footer-top footer-padding">
+              <div className="row justify-content-center">
+                <div className="col-lg-6">
+                  <div className="footer-top-cap text-center">
+                    <img src="assets/img/logo/xlogo2_footer.png.pagespeed.ic.b9mPVtZSbx.png" alt="" />
+                    <span><a href="#"><span className="__cf_email__" data-cfemail="2f47465d4a424a6f5f405d5b4940434640014c4042">[email&nbsp;protected]</span></a></span>
+                    <p>221B Baker Street, Post office Box 353 </p>
+                    <p>Park Road, USA - 215431</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="footer-bottom">
+              <div className="row d-flex justify-content-between align-items-center">
+                <div className="col-xl-9 col-lg-8">
+                  <div className="footer-copy-right">
+                    <p>
+                      Copyright ©
+                      All rights reserved | This template is made
+                      with <i className="fa fa-heart" aria-hidden="true" /> by <a href="https://colorlib.com/" target="_blank">Colorlib</a>
+                    </p>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-4">
+                  <div className="footer-social f-right">
+                    <a>Stay Connected</a>
+                    <a href="#"><i className="fab fa-twitter" /></a>
+                    <a href="#"><i className="fab fa-facebook-f" /></a>
+                    <a href="#"><i className="fas fa-globe" /></a>
+                    <a href="#"><i className="fab fa-instagram" /></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </footer>
- </>
+    </>
   )
 }
 
