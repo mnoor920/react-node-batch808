ac0c2791fefa83790f2d2adcdcb304c091fbe773
mnoor920 <mnoor92081@hotmail.com>
2025-08-10 20:08:44 +0500

    style setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './style/style.css';
+import './assets/css/style.css';
+import './assets/css/A_bootstrap_min.css';
+import './assets/css/A_bootstrap.css';
+import './assets/css/A2_bootstrap.css';
+import './assets/css/style_2.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
